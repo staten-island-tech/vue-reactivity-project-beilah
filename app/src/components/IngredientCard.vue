0420ddc7c8ffce9097d3f50d03357d351e55baf1
Beila <beilah@nycstudents.net>
2026-02-13 13:46:24 -0500

    aasd

---
+++ app/src/components/IngredientCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <h2>{{ ingredient.name }}</h2>
+    <div class="image-wrapper">
+      <img :src="ingredient.img" :alt="ingredient.name" />
+    </div>
+    <p>{{ ingredient.price }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+    ingredient: {
+        type: Object,
+        required: true,
+    }
+})
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  padding: 16px;
+  border-radius: 8px;
+  width: 250px;
+  height: 25s0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.image-wrapper {
+  width: 100%;
+  height: 180px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.image-wrapper img {
+  width: 80%;
+  height: 80%;
+  object-fit: contain;
+}
+
+</style>
--- app/src/views/MakeRamen.vue
+++ app/src/views/MakeRamen.vue
@@ -1,11 +1,74 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Ramen Maker</h1>
-    <img src="" alt="">
-  </div>
+    <img src="/EmptyBowl.png" alt="Empty Bowl Img Holder">
 
+    <div class="card-grid">
+      <IngredientCard
+        v-for="ingredient in ingredients"
+        :key="ingredient.name"
+        :ingredient="ingredient"
+      />
+    </div>
+
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import IngredientCard from '@/components/IngredientCard.vue'
+const ingredients = ref([
+  { name: 'Egg', img: '/Egg.png', price: '$2' },
+  { name: 'Ham', img: '/Ham.png', price: '$2' },
+  { name: 'Narutomaki', img: '/Narutomaki.png', price: '$2' },
+  { name: 'Seaweed', img: '/Seaweed.png', price: '$2' },
+])
+</script>
 
-<style scoped></style>
+<style scoped>
+img {
+  width: 600px;
+  height: auto;
+  display: block;
+  margin-top: 20px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80vw;
+  margin: 20px auto;
+  justify-content: space-around;
+  height: 60vh;
+    flex-wrap: wrap;
+}
+img {
+  width: 600px;
+  height: auto;
+  margin: 20px 0;
+}
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); /* 2 columns */
+  gap: 20px;
+  justify-items: center;
+}
+div {
+  padding: 16px;
+}
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+.card {
+  border: 1px solid #ccc;
+  padding: 12px;
+  margin-bottom: 12px;
+  border-radius: 6px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
